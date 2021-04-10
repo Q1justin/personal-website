ee8fb225d8338d6a9b68ef69de37b85c87476467
Q1justin <q1justindo@gmail.com>
2021-04-09 21:48:48 -0400

    Making further big changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React, { Component } from 'react';
 import './App.css';
-import ContactBar from './components/Navigation/ContactBar/ContactBar';
+// import ContactBar from './components/Navigation/ContactBar/ContactBar';
 import WelcomePage from './components/WelcomePage/WelcomePage';
-import AboutMe from './components/AboutMe/AboutMe';
-import Projects from './components/Projects/Projects';
-import TestContainer from './containers/TestContainer';
-import ScrollBar from './components/Navigation/ScrollBar/ScrollBar';
+// import AboutMe from './components/AboutMe/AboutMe';
+// import Projects from './components/Projects/Projects';
+// import TestContainer from './containers/TestContainer';
+// import ScrollBar from './components/Navigation/ScrollBar/ScrollBar';
 
-function App() {
-  return (
-    <body>
-      <WelcomePage />
-    </body>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <WelcomePage />
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,20 +1,32 @@
 import React, {Component} from 'react';
 import classes from './WelcomePage.module.css';
 
-class WelcomePage extends Component{
+class welcomePage extends Component{
+  state = {
+    isClicked: false
+  }
+
+  handleClick(){
+    this.setState({
+      isClicked: true
+    })
+  }
+
   render(){
     return (
-      <section className = {classes.home}>
-        <div className = {classes.welcomeText}>
+      <section className = {this.state.isClicked ? {} : classes.home}>
+        <div className = {classes.welcomeText} style={this.state.isClicked ? { transform: `translate(200%)` } : {}}>
           <h1>Hi, My Name Is Gyuwon Do</h1>
           <h2>안녕하세요, 도규원입니다</h2>
         </div>
-        <div className={classes.buttonContainer}>
-          <button>Click Me!</button>
+        <div className={classes.buttonContainer} style={this.state.isClicked ? { transform: `translate(200%)` } : {}}>
+          <button onClick = {() => this.handleClick()}>
+            Click Me!
+          </button>
         </div>
       </section>
     )
   }
 }
 
-export default WelcomePage;
+export default welcomePage;
