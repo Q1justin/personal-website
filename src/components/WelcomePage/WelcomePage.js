44a6e4b4d002d922172b018af4fa9feeaf4bf9d0
Q1justin <q1justindo@gmail.com>
2021-04-09 22:38:13 -0400

    Showing and exploring animations

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -3,18 +3,22 @@ import classes from './WelcomePage.module.css';
 
 class welcomePage extends Component{
   state = {
-    isClicked: false
+    isClicked: false,
+    showDiv: false
   }
 
   handleClick(){
     this.setState({
       isClicked: true
     })
+    setTimeout(function() { //Start the timer
+        this.setState({showDiv: true}) //After 1 second, set render to true
+    }.bind(this), 2000)
   }
 
   render(){
     return (
-      <section className = {this.state.isClicked ? {} : classes.home}>
+      <section className = {classes.home}>
         <div className = {classes.welcomeText} style={this.state.isClicked ? { transform: `translate(200%)` } : {}}>
           <h1>Hi, My Name Is Gyuwon Do</h1>
           <h2>안녕하세요, 도규원입니다</h2>
@@ -23,6 +27,14 @@ class welcomePage extends Component{
           <button onClick = {() => this.handleClick()}>
             Click Me!
           </button>
+        </div>
+        <div style={this.state.showDiv ? {} : { display: `none` }}>
+          <h1>WHATWHAT</h1>
+          <h1>WHATWHAT</h1>
+          <h1>WHATWHAT</h1>
+          <h1>WHATWHAT</h1>
+          <h1>WHATWHAT</h1>
+          <h1>WHATWHAT</h1>
         </div>
       </section>
     )
